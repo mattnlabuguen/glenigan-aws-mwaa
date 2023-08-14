c41b42e64a009d44f069b02d9803af19e0515e2f
Rene Matthew Labuguen <r.labuguen@bcicentral.com>
2023-08-15 00:21:12 +0800

    Fix: import errors

--- dags/glenigan_scrapers/wandsworth_dag.py
+++ dags/glenigan_scrapers/wandsworth_dag.py
@@ -1,8 +1,7 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
-from plugins.strategies.utils.strategy_utils import get_crawling_strategy, get_parsing_strategy
-
+from ..plugins.strategies.utils.strategy_utils import get_crawling_strategy, get_parsing_strategy
 
 default_args = {
     'owner': 'BCI Central'
